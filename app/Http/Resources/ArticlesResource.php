bb619a935e386c582698af7a5377c1859ddafffd
ruslanbelchenkov <troum@outlook.com>
2024-03-29 22:00:22 +0300

    Update

--- app/Http/Resources/ArticlesResource.php
+++ app/Http/Resources/ArticlesResource.php
@@ -22,7 +22,7 @@ class ArticlesResource extends JsonResource
         return [
             'title' => $this->resource['attributes']['title'],
             'description' => $this->resource['attributes']['description'],
-            'image' => $this->resource['attributes']['image']['data'],
+            'image' => $this->resource['attributes']['image']['data']['attributes']['url'],
             'date' => $this->resource['attributes']['createdAt'],
             'seo' => new SeoResource($this->resource['attributes']['seo']),
         ];
