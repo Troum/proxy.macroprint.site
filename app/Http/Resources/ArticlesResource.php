b5dff96580a244791b2a75f0afe0fcda390b7c73
ruslanbelchenkov <troum@outlook.com>
2024-03-29 22:41:58 +0300

    Update{

--- app/Http/Resources/ArticlesResource.php
+++ app/Http/Resources/ArticlesResource.php
@@ -27,7 +27,7 @@ class ArticlesResource extends JsonResource
             ],
             'date' => $this->resource->article['createdAt'],
             'seo' => new SeoResource($this->resource->article['seo']),
-            'others' => $this->when(array_key_exists('others', $this->resource->article), function () {
+            'others' => $this->when(property_exists($this->resource, 'others'), function () {
                 $others = collect($this->resource->others)->map(function ($item) {
                     $other = new \stdClass();
                     $other->article = $item;
